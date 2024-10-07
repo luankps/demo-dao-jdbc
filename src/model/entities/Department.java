520598b1a65ce47c997230f2f0117eee722c25cc
Luan Sousa <luankps@gmail.com>
2024-10-07 15:50:08 -0300

    Final

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -51,10 +51,10 @@ public class Department implements Serializable {
 
     @Override
     public String toString() {
-        return "Department{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
+        return "Department " +
+                "[id = " + id +
+                ", name = '" + name + '\'' +
+                ']';
     }
 
 }
